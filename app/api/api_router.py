0444b268c5500f66b8aaeb586e12a6da8ea67eff
최영우 <uoise@wisenut.co.kr>
2025-04-10 05:42:23 +0000

    Resolve "메타데이터 구조 관련 없는 것 전부 제거"

--- app/api/api_router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, Depends
-from starlette.responses import JSONResponse
-
-from app.dependencies import get_token_header
-
-api_router = APIRouter(dependencies=[Depends(get_token_header)], default_response_class=JSONResponse)
