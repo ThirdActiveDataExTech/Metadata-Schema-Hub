b2ff9cf6bc13b7bc0bcc77aa989620a882f1d160
uoise1 <uoise@wisenut.co.kr>
2025-10-01 17:08:26 +0900

    FEAT: add tx on SessionDep dependencies

--- apps/catalog-service/app/dependencies.py
+++ apps/catalog-service/app/dependencies.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Generator
 
-from fastapi import Security, Depends
+from fastapi import Depends, Security
 from fastapi.security import APIKeyHeader
 from sqlmodel import Session
 
@@ -17,7 +17,12 @@ async def get_token_header(x_token: Annotated[str, Security(header_scheme)]):
 def get_session() -> Generator[Session, None, None]:
     """Get session"""
     with Session(engine) as session:
-        yield session
+        try:
+            yield session
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
