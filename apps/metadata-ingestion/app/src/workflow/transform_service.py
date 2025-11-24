88aa073a15b7c77f5b099b17e9a278ef2d50005e
uoise1 <uoise@wisenut.co.kr>
2025-11-24 11:15:49 +0900

    FIX: remove unused methods from transform service

--- apps/metadata-ingestion/app/src/workflow/transform_service.py
+++ apps/metadata-ingestion/app/src/workflow/transform_service.py
@@ -1,14 +1,12 @@
 import logging
-from datetime import datetime
-from typing import Dict, Iterable, List, Tuple
+from typing import List
 
 from app.dependencies import SessionDep
-from app.src.catalog_entry.model import CatalogEntry, CatalogEntrySummary, CatalogEntryUpdate
+from app.src.catalog_entry.model import CatalogEntry, CatalogEntryUpdate
 from app.src.catalog_entry.service import CatalogEntryService
 from app.src.column_relation.model import ColumnRelation
 from app.src.column_relation.service import ColumnRelationService
-from app.src.file_converter.file_handler import MetadataFile, process_metadata_file, process_metadata_files
-from app.src.metadata_entry.model import MetadataBase, MetadataCreate
+from app.src.metadata_entry.model import MetadataBase
 from app.src.metadata_entry.service import MetadataEntryService
 
 
@@ -54,95 +52,6 @@ class CatalogEntryTransformService:
                 processed_columns.add(relation.catalog_column)
 
         return catalog_entry_update.apply_to_catalog_entry(catalog_entry=catalog_entry)
-
-    def create_metadata_catalog_entry_from_metadata(
-        self,
-        db: SessionDep,
-        file: MetadataFile,
-    ) -> CatalogEntry:
-        """메타데이터 파일을 처리하여 CatalogEntry 생성."""
-        # TODO: unused method
-        serialized_content, metadata_bases = process_metadata_file(file)
-        ingested_at = datetime.now()
-
-        metadata_create = MetadataCreate(metadata_bases=metadata_bases, ingested_at=ingested_at)
-        catalog_entry = CatalogEntry(
-            identifier=metadata_create.metadata_id,
-            raw_metadata=serialized_content,
-            ingested_at=ingested_at,
-        )
-
-        with db.begin():
-            related_relations = self.column_relation_service.get_relations_by_metadata_columns(
-                db=db,
-                metadata_columns=[metadata_base.metadata_schema for metadata_base in metadata_bases],
-            )
-
-            catalog_entry = self._set_catalog_entry_fields(
-                catalog_entry=catalog_entry,
-                metadata_bases=metadata_bases,
-                related_relations=related_relations,
-                preserve_existing=False,  # 새로 생성하므로 기존 필드 유지 안함
-            )
-
-            self.metadata_entry_service.create(db=db, metadata_create=metadata_create)
-            self.catalog_entry_service.create_catalog_entry(
-                db=db,
-                catalog_entry=catalog_entry,
-            )
-
-        return self.catalog_entry_service.get_catalog_entry_by_identifier(db, catalog_entry.identifier)
-
-    def create_metadata_catalog_entries_from_metadatas(
-        self,
-        db: SessionDep,
-        files: List[MetadataFile],
-    ) -> Tuple[List[CatalogEntrySummary], List[Dict[str, str]]]:
-        """여러 메타데이터 파일을 처리하여 CatalogEntry 리스트 생성."""
-        # TODO: unused method
-        processed_metadatas, errors = process_metadata_files(files)
-        if not processed_metadatas:
-            logging.warning("Empty files in bulk transform.")
-            return [], errors
-
-        ingested_at = datetime.now()
-
-        iterrables = []
-        metadata_schemas: Iterable[str] = set()
-        for serialized_content, metadata_bases in processed_metadatas:
-            metadata_create = MetadataCreate(metadata_bases=metadata_bases, ingested_at=ingested_at)
-            catalog_entry = CatalogEntry(
-                identifier=metadata_create.metadata_id,
-                raw_metadata=serialized_content,
-                ingested_at=ingested_at,
-            )
-            iterrables.append((catalog_entry, metadata_create))
-            metadata_schemas.update(e.metadata_schema for e in metadata_bases)
-
-        with db.begin():
-            related_relations = self.column_relation_service.get_relations_by_metadata_columns(
-                db=db,
-                metadata_columns=metadata_schemas,
-            )
-
-            for catalog_entry, metadata_create in iterrables:
-                catalog_entry = self._set_catalog_entry_fields(
-                    catalog_entry=catalog_entry,
-                    metadata_bases=metadata_create.metadata_bases,
-                    related_relations=related_relations,
-                    preserve_existing=False,
-                )
-
-            self.metadata_entry_service.create_bulk(
-                db, metadata_create_list=[metadata_create for _, metadata_create in iterrables]
-            )
-            self.catalog_entry_service.create_catalog_entry_bulk(
-                db, catalog_entries=[entry.model_dump() for entry, _ in iterrables]
-            )
-
-        return self.catalog_entry_service.get_catalog_entry_summary_by_identifier(
-            db, [entry.identifier for entry, _ in iterrables]
-        ), errors
 
     def update_catalog_entry_from_metadata_and_relation(
         self,
